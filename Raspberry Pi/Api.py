e3cb050ef80f6ed9a081c964f6f6d60375f66a84
Kostiantyn Dovbush <kostiantyn.dovbush@gmail.com>
2017-02-15 19:03:54 +0100

    Fixed minor bug

--- Raspberry Pi/Api.py
+++ Raspberry Pi/Api.py
@@ -13,34 +13,34 @@ def check():
     data["status"] = "true"
     return Response(json.dumps(data), status=200, mimetype="application/json")
 
-@app.route('weatherStation/api/v1.0/detectors', methods=['GET'])
+@app.route('/weatherStation/api/v1.0/detectors', methods=['GET'])
 def getDetectors():
     detectors = DatabaseManager().getDetectors()
     data = {}
     data["detectors"] = JSONParser.encodeArrayOfDetectors(detectors)
     return Response(json.dumps(data), status=200, mimetype="application/json")
 
-@app.route('weatherStation/api/v1.0/detectors/<int:identifier>/measurements', methods=['GET'])
+@app.route('/weatherStation/api/v1.0/detectors/<int:identifier>/measurements', methods=['GET'])
 def getMeasurementsByDetectorId(identifier):
     measurements = DatabaseManager().getMeasurementsForDetectorId(identifier)
     data = {}
     data["measurements"] = JSONParser.encodeArrayOfMeasurements(measurements)
     return Response(json.dumps(data), status=200, mimetype="application/json")
 
-@app.route('weatherStation/api/v1.0/detectors/<int:identifier>/measurementsAfter/<float:timestamp>', methods=['GET'])
+@app.route('/weatherStation/api/v1.0/detectors/<int:identifier>/measurementsAfter/<float:timestamp>', methods=['GET'])
 def getMeasurementsByDetectorIdAfter(identifier, timestamp):
     measurements = DatabaseManager().getMeasurementsForDetectorIdAfter(identifier, timestamp)
     data = {}
     data["measurements"] = JSONParser.encodeArrayOfMeasurements(measurements)
     return Response(json.dumps(data), status=200, mimetype="application/json")
 
-@app.route('weatherStation/api/v1.0/measurements/clean', methods=['GET'])
+@app.route('/weatherStation/api/v1.0/measurements/clean', methods=['GET'])
 def clean():
     data = {}
     data["status"] = DatabaseManager().cleanAllMeasurements()
     return Response(json.dumps(data), status=200, mimetype="application/json")
 
-@app.route('weatherStation/api/v1.0/detectors/<int:identifier>/clean', methods=['GET'])
+@app.route('/weatherStation/api/v1.0/detectors/<int:identifier>/clean', methods=['GET'])
 def cleanDetector(identifier):
     data = {}
     data["status"] = DatabaseManager().cleanMeasurementsForDetectorId(identifier)
